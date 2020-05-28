73f4af9df1f3f740859457c66ea802bbbdb48785
Marco Marani <m.marani@crif.com>
2020-05-28 22:47:54 +0200

    WIP added reset to sliders

--- frontend/webcam-to-pdf/src/components/FilterPane.jsx
+++ frontend/webcam-to-pdf/src/components/FilterPane.jsx
@@ -7,37 +7,47 @@ export default class FilterPane extends Component {
         this.state = {
             image: "",
 
-            grayscale: false,
             brightness: 1.0,
             contrast: 1.0,
-            saturation: 1.0,
-            hue: 0
+            saturation: 1.0
         }
+
+        this._img = React.createRef()
     }
 
     render() {
         let imgStyles = {
             margin: 'auto',
             position: 'relative',
-            top: 50,
+            top: 54,
             border: '1px',
             borderColor: 'black',
-            width: 200,
-            height: 150
+            width: 400,
+            height: 300
         }
+        
+        let controllerContainerStyles = {
+            position: "relative",
+            top: 50,
+        }
+
+        /* TODO we must extract the filtered
+        https://stackoverflow.com/questions/30408939/how-to-save-image-from-canvas-with-css-filters
+        */
+        let filters = "brightness(" + this.state.brightness + ") " +
+            "contrast(" + this.state.contrast + ") "
+            
+        if (this.state.saturation == 0.0) {
+            filters = filters + "grayscale(100%)"
+        } else {
+            filters = filters + "saturate("+this.state.saturation+") " 
+        }
+
+        imgStyles.filter = filters
 
         return (
             <div className="row">
-                <div className="width40">
-                    <div className="row">
-                        <div className="text-right">Grayscale:</div>
-                        <input className="margin10"
-                            type="checkbox"
-                            id="grayscale"
-                            name="grayscale"
-                            value={this.state.grayscale}
-                            onChange={this.handleValueChanged} />
-                    </div>
+                <div className="width40" style={controllerContainerStyles}>
                     <div className="row">
                         <div className="width50 text-right">Contrast:</div>
                         <input className="margin10"
@@ -68,25 +78,24 @@ export default class FilterPane extends Component {
                             value={this.state.saturation}
                             onChange={this.handleValueChanged} />
                     </div>
-                    <div className="row">
-                        <div className="width50 text-right">Hue:</div>
-                        <input className="margin10"
-                            type="range" min="0"
-                            step="1"
-                            max="360"
-                            name="hue"
-                            value={this.state.hue}
-                            onChange={this.handleValueChanged} />
-                    </div>
+                    <button className="btn btn-primary width70 margin10" onClick={this.resetSliders}>Reset</button>
                 </div>
                 <div className="width60">
-                    { this.state.image && (<img style={imgStyles} src={this.state.image} />)}
+                    {this.state.image && (<img ref={this._img} style={imgStyles} src={this.state.image} />)}
                 </div>
             </div>
         )
     }
 
+    resetSliders = () => {
+        this.setState({brightness: 1.0, contrast : 1.0, saturation : 1.0 })
+    }
+
     handleValueChanged = (event) => {
+        if (event.target.name === "grayscale") {
+            alert(event.target.value)
+        }
+
         this.setState({
             [event.target.name]: event.target.value
         })
