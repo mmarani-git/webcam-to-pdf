c559774bdc972b6316130ec5bc20b1ee0329d504
Marco Marani <m.marani@crif.com>
2020-05-28 22:09:46 +0200

    unified component for filter

--- frontend/webcam-to-pdf/src/components/FilterPane.jsx
+++ frontend/webcam-to-pdf/src/components/FilterPane.jsx
@@ -1,144 +1,94 @@
 import React, { Component } from 'react'
-//import ImageService from '../services/ImageService.js'
+import ImageService from '../services/ImageService.js'
 
 export default class FilterPane extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            image: ""
-        }
-    }
+            image: "",
 
-    render() {
-        return (
-            <div className="row">
-                <div className="width40"><Controls /></div>
-                <div className="width60"><Canvas image={this.state.image} /></div>
-            </div>
-        )
-    }
-
-    addScreenshoot(image) {
-        this.setState({ image: image })
-    }
-}
-
-class Controls extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
             grayscale: false,
-            contrast: 0,
-            brightness: 0,
-            saturation: 0,
+            brightness: 1.0,
+            contrast: 1.0,
+            saturation: 1.0,
             hue: 0
         }
     }
 
     render() {
-        return (<>
-            <div className="row">
-                <div className="text-right">
-                    Grayscale:
-            </div>
-                <input className="margin10"
-                    type="checkbox"
-                    id="grayscale"
-                    name="grayscale"
-                    value={this.state.grayscale}
-                    onChange={this.handleValueChanged} />
-            </div>
-            <div className="row">
-                <div className="width50 text-right">
-                    Contrast:
-                </div>
-                <input className="margin10"
-                    type="range" min="-100"
-                    step="1"
-                    max="100"
-                    vaule={this.state.contrast}
-                    onChange={this.handleValueChanged} />
-            </div>
-            <div className="row">
-                <div className="width50 text-right">
-                    Brightness:
-                </div>
-                <input className="margin10"
-                    type="range" min="-100"
-                    step="1"
-                    max="100"
-                    vaule={this.state.brightness}
-                    onChange={this.handleValueChanged} />
-            </div>
-            <div className="row">
-                <div className="width50 text-right">
-                    Saturation:
-                </div>
-                <input className="margin10"
-                    type="range" min="-100"
-                    step="1"
-                    max="100"
-                    vaule={this.state.saturation}
-                    onChange={this.handleValueChanged} />
-            </div>
-            <div className="row">
-                <div className="width50 text-right">
-                    Hue:
-                </div>
-                <input className="margin10"
-                    type="range" min="-100"
-                    step="1"
-                    max="100"
-                    vaule={this.state.hue}
-                    onChange={this.handleValueChanged} />
-            </div>
-        </>)
-    }
-}
-
-const CANVAS_HEIGHT = 200
-
-class Canvas extends Component {
-
-    constructor(props) {
-        super(props)
-        this._canvas = React.createRef();
-    }
-
-    render() {
-        const canvasStyles = {
+        let imgStyles = {
             margin: 'auto',
             position: 'relative',
             top: 50,
             border: '1px',
-            borderColor: 'black'
+            borderColor: 'black',
+            width: 200,
+            height: 150
         }
 
-        let canvas = this._canvas.current
-        
-        if (canvas !== null) {
-            this.updateCanvas(canvas);
-        }
-
-        return (<canvas ref={this._canvas} id="canvas" style={canvasStyles}></canvas>)
+        return (
+            <div className="row">
+                <div className="width40">
+                    <div className="row">
+                        <div className="text-right">Grayscale:</div>
+                        <input className="margin10"
+                            type="checkbox"
+                            id="grayscale"
+                            name="grayscale"
+                            value={this.state.grayscale}
+                            onChange={this.handleValueChanged} />
+                    </div>
+                    <div className="row">
+                        <div className="width50 text-right">Contrast:</div>
+                        <input className="margin10"
+                            type="range" min="0.0"
+                            step="0.01"
+                            max="2.0"
+                            vaule={this.state.contrast}
+                            onChange={this.handleValueChanged} />
+                    </div>
+                    <div className="row">
+                        <div className="width50 text-right">Brightness:</div>
+                        <input className="margin10"
+                            type="range" min="0.0"
+                            step="0.01"
+                            max="2.0"
+                            vaule={this.state.brightness}
+                            onChange={this.handleValueChanged} />
+                    </div>
+                    <div className="row">
+                        <div className="width50 text-right">Saturation:</div>
+                        <input className="margin10"
+                            type="range" min="0.0"
+                            step="0.01"
+                            max="2.0"
+                            vaule={this.state.saturation}
+                            onChange={this.handleValueChanged} />
+                    </div>
+                    <div className="row">
+                        <div className="width50 text-right">Hue:</div>
+                        <input className="margin10"
+                            type="range" min="0"
+                            step="1"
+                            max="360"
+                            vaule={this.state.hue}
+                            onChange={this.handleValueChanged} />
+                    </div>
+                </div>
+                <div className="width60">
+                    { this.state.image && (<img style={imgStyles} src={this.state.image} />)}
+                </div>
+            </div>
+        )
     }
 
-    updateCanvas = (canvas) => {
-        let ctx = canvas.getContext('2d');
+    handleValueChanged = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
 
-        /*
-        UNDEFINED: WHY ??????????????????????????????????????????
-        THIS IS SYNC
-        let w = ImageService.getWidthFromNewHeight(this.props.image, CANVAS_HEIGHT)
-        */
-        let w = CANVAS_HEIGHT / 3 * 4
-        canvas.width = w
-        canvas.height = CANVAS_HEIGHT
-
-        let image = new Image()
-        image.onload = function () {
-            ctx.drawImage(image, 0, 0, w, CANVAS_HEIGHT)
-        }
-        image.src = this.props.image
+    addScreenshoot(image) {
+        this.setState({ image: image })
     }
 }
